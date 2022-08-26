2763c9ccfdf98b2256462a8889fed1346db68e99
lnv153 <omkar.revankar@thinkitive.com>
2022-08-26 14:34:13 +0530

    changed some links

--- public/js/app.js
+++ public/js/app.js
@@ -10,22 +10,22 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const inputFields = search.value;
 
-  fetch(`http://localhost:3000/weather?address=${inputFields}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          weatherUpdate.textContent = data.location;
-          // console.log(data.error);
-        } else {
-          messageOne.textContent = data.location;
-          weatherUpdate.textContent = data.forecast;
-          // console.log(data.location);
-          // console.log(data.forecast);
-        }
-      });
-    }
-  );
+  fetch(
+    `https://weather-app76.herokuapp.com/weather?address=${inputFields}`
+  ).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        weatherUpdate.textContent = data.location;
+        // console.log(data.error);
+      } else {
+        messageOne.textContent = data.location;
+        weatherUpdate.textContent = data.forecast;
+        // console.log(data.location);
+        // console.log(data.forecast);
+      }
+    });
+  });
 
   // console.log(search.value);
 });
